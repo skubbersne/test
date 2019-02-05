eeaf0bb13c6288e544a982e0bb697cece587abe2
skubbersne <47176205+skubbersne@users.noreply.github.com>
2019-02-05 15:01:49 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -60,7 +60,7 @@ function refreshDeviceList(){
 
 function onDiscoverDevice(device){
 	//Make a list in html and show devises
-		if(device.name = "CLAUS") {
+	if(device.name == "CLAUS") {
 		var listItem = document.createElement('li'),
 		html = device.name+ "," + device.id;
 		listItem.innerHTML = html;
